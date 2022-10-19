23ffd36c729e6967258ad9e6005fc8905fda45ec
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-19 11:53:25 +0700

    week2: 10-wrap HomeScreen and ProfileScreen with BlocBuilder

--- finish/week2_state_manager/lib/ui/profile/profile_screen.dart
+++ finish/week2_state_manager/lib/ui/profile/profile_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:week2_state_manager/ui/cubit/auth_cubit.dart';
 import 'package:week2_state_manager/ui/widgets/app_button.dart';
+import 'package:week2_state_manager/utils/status.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AppButton(
-        caption: 'Logout email@gmail.com',
-        onClick: () => null,
-      ),
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (context, state) {
+        if (state is UserAuthState && state.status == Status.success) {
+          return Center(
+            child: AppButton(
+              caption: 'Logout ${state.data?.email}',
+              onClick: () => null,
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
