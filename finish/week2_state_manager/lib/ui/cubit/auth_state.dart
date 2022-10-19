68d86112b9e6f40b7baabc60b26e7193149711e7
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-19 11:21:05 +0700

    week2: 6-create UserAuthState

--- finish/week2_state_manager/lib/ui/cubit/auth_state.dart
+++ finish/week2_state_manager/lib/ui/cubit/auth_state.dart
@@ -4,7 +4,21 @@ abstract class AuthState extends Equatable {
   const AuthState();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 class AuthInitial extends AuthState {}
+
+class UserAuthState extends AuthState {
+  final Status status;
+  final String? message;
+  final User? data;
+
+  const UserAuthState({
+    required this.status,
+    this.message,
+    this.data,
+  });
+  @override
+  List<Object?> get props => [status, message, data];
+}
