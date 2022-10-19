8dfaa193d62c1effaf4e06e37ec927be7fe62c07
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-19 11:56:47 +0700

    week2: 12-change BlocBuilder to BlocConsumer in ProfileScreen and implement logout

--- finish/week2_state_manager/lib/ui/profile/profile_screen.dart
+++ finish/week2_state_manager/lib/ui/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:week2_state_manager/ui/cubit/auth_cubit.dart';
 import 'package:week2_state_manager/ui/widgets/app_button.dart';
 import 'package:week2_state_manager/utils/status.dart';
@@ -9,13 +10,18 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthCubit, AuthState>(
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is UserLogoutState) {
+          context.go('/');
+        }
+      },
       builder: (context, state) {
         if (state is UserAuthState && state.status == Status.success) {
           return Center(
             child: AppButton(
               caption: 'Logout ${state.data?.email}',
-              onClick: () => null,
+              onClick: () => context.read<AuthCubit>().logout(),
             ),
           );
         }
