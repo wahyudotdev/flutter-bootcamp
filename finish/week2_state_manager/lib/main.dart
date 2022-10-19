56520f5604ab504b856c1a647fe64f06cc938c8d
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-19 11:36:43 +0700

    week2: 8-wrap entire material app in BlocProvider

--- finish/week2_state_manager/lib/main.dart
+++ finish/week2_state_manager/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:week2_state_manager/data/api.dart';
+import 'package:week2_state_manager/ui/cubit/auth_cubit.dart';
 import 'package:week2_state_manager/ui/router.dart';
 
 void main() {
@@ -10,15 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => AuthCubit(ApiImpl()),
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routerDelegate: router.routerDelegate,
+        routeInformationParser: router.routeInformationParser,
+        routeInformationProvider: router.routeInformationProvider,
       ),
-      routerDelegate: router.routerDelegate,
-      routeInformationParser: router.routeInformationParser,
-      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
