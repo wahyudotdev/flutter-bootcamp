791a5d67d90d622d521768ff2bb9260162a73e40
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-19 11:36:15 +0700

    week2: 7-implement login logic in cubit

--- finish/week2_state_manager/lib/ui/cubit/auth_cubit.dart
+++ finish/week2_state_manager/lib/ui/cubit/auth_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:week2_state_manager/data/api.dart';
+import 'package:week2_state_manager/models/user.dart';
+import 'package:week2_state_manager/utils/status.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  AuthCubit(this._api) : super(AuthInitial());
+
+  final Api _api;
+
+  void login({required String email, required String password}) async {
+    emit(const UserAuthState(status: Status.loading));
+    try {
+      final response = await _api.login(email, password);
+      emit(UserAuthState(status: Status.success, data: response));
+    } catch (e) {
+      emit(UserAuthState(status: Status.error, message: e.toString()));
+    }
+  }
 }
