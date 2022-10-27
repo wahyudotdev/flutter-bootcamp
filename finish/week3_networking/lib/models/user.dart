b8b055036d44f8e4f6db4f7ec66268e4cdcca609
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-27 13:49:12 +0700

    week3: 3 - create factory method for User

--- finish/week3_networking/lib/models/user.dart
+++ finish/week3_networking/lib/models/user.dart
@@ -6,4 +6,7 @@ class User {
     required this.name,
     required this.email,
   });
+
+  factory User.fromJson(Map<String, dynamic> json) =>
+      User(name: json['name'], email: json['email']);
 }
