bad6a39f390a8d4039b1b3b02759c1f5471dfd8f
wahyudotdev <wahyudotdev@gmail.com>
2022-11-10 17:34:23 +0700

    week4: 3 - register dependency graph

--- finish/week4_dependency_injection/lib/main.dart
+++ finish/week4_dependency_injection/lib/main.dart
@@ -1,17 +1,11 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
-import 'package:dio_logging_interceptor/dio_logging_interceptor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:week4_dependency_injection/config/config.dart';
-import 'package:week4_dependency_injection/data/api.dart';
+import 'package:week4_dependency_injection/di/injection.dart';
 import 'package:week4_dependency_injection/ui/cubit/auth_cubit.dart';
 import 'package:week4_dependency_injection/ui/router.dart';
-import 'package:week4_dependency_injection/utils/token_interceptor.dart';
 
 void main() {
+  configureDependencies();
   runApp(const MyApp());
 }
 
@@ -20,13 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final platform = Platform.isAndroid ? 'android' : 'ios';
-    final dio = Dio(
-        BaseOptions(baseUrl: Config.baseUrl, headers: {'platform': platform}));
-    dio.interceptors.add(TokenInterceptor(const FlutterSecureStorage()));
-    dio.interceptors.add(DioLoggingInterceptor(level: Level.body));
     return BlocProvider(
-      create: (context) => AuthCubit(Api(dio)),
+      create: (context) => getIt<AuthCubit>(),
       child: MaterialApp.router(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
