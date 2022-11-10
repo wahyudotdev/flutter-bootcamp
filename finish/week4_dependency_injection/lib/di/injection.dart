6d4c5d668dcda1f361ee199fc22d587b69cf7ced
wahyudotdev <wahyudotdev@gmail.com>
2022-11-10 17:43:02 +0700

    week4: 5 - configure injectable initialization

--- finish/week4_dependency_injection/lib/di/injection.dart
+++ finish/week4_dependency_injection/lib/di/injection.dart
@@ -1,24 +1,8 @@
-import 'package:dio/dio.dart';
-import 'package:dio_logging_interceptor/dio_logging_interceptor.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
-import 'package:week4_dependency_injection/config/config.dart';
-import 'package:week4_dependency_injection/data/api.dart';
-import 'package:week4_dependency_injection/ui/cubit/auth_cubit.dart';
-import 'package:week4_dependency_injection/utils/token_interceptor.dart';
+import 'package:injectable/injectable.dart';
+import 'package:week4_dependency_injection/di/injection.config.dart';
 
 final getIt = GetIt.instance;
 
-void configureDependencies() {
-  getIt.registerSingleton(TokenInterceptor(const FlutterSecureStorage()));
-
-  getIt.registerLazySingleton<Dio>(() {
-    final dio = Dio(BaseOptions(baseUrl: Config.baseUrl));
-    dio.interceptors.add(getIt<TokenInterceptor>());
-    dio.interceptors.add(DioLoggingInterceptor(level: Level.body));
-    return dio;
-  });
-
-  getIt.registerSingleton<Api>(Api(getIt()));
-  getIt.registerFactory(() => AuthCubit(getIt()));
-}
+@injectableInit
+void configureDependencies() => $initGetIt(getIt);
